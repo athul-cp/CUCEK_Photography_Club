8066ae9397d198214a30495d634219f27c800deb
athulcp_ <athulcp003@gmail.com>
2021-10-29 01:33:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-$(document).ready(function()
-{
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        if(this.scroll>500){
-            $('.scroll-up-button').addClass("show");
-        }else{
-            $('.scroll-up-button').removeClass("show");
-        }
-         
-    });
-});
+
